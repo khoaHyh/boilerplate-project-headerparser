469b69c2a928838180096c4c7ca5b7b293ccfc24
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-12 00:04:41 -0500

    Feat: Add potential solution to project tests

--- server.js
+++ server.js
@@ -19,15 +19,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// https://stackoverflow.com/questions/8107856/how-to-determine-a-users-ip-address-in-node
+// processing x-forwarded-for only if set, if so, take the first address, other params use
+// optional chaining
+const parseIp = (req) =>
+    (typeof req.headers['x-forwarded-for'] === 'string'
+        && req.headers['x-forwarded-for'].split(',').shift())
+    || req.connection?.remoteAddress
+    || req.socket?.remoteAddress
+    || req.connection?.socket?.remoteAddress
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  // could use req.ip from express but will return a variation of 127.0.0.1 
+  // if the server is running behind a proxy
+  res.status(200).json({ 
+    "ipaddress": parseIp(req), 
+    "language": req.header("Accept-Language"), 
+    "software": req.header("User-Agent") 
+  });
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
